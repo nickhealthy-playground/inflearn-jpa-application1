0cc1ffbc05a999c73f788b4b523e487d08b3ef93
nickhealthy <alskadmlcraz1@gmail.com>
2024-12-29 17:17:58 +0900

    2. 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    private List<Item> items = new ArrayList<>();
+}
