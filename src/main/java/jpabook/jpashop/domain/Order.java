4132caf130fb508b74721d5f63abc128a9066be3
nickhealthy <alskadmlcraz1@gmail.com>
2024-12-31 15:24:05 +0900

    6. 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -50,4 +50,55 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    //연관된 데이터가 많을 경우 다음과 같은 static 메서드로 별도로 분리하는 것이 좋다.
+    /**
+     * 주문 엔티티를 생성할 때 사용한다.
+     * 주문 회원, 배송정보, 주문상품의 정보를 받아서 실제 주문 엔티티를 생성한다.
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     * 주문 상태를 취소로 변경하고 주문상품에 주문 취소를 알린다.
+     * 만약 이미 배송을 완료한 상품이면 주문을 취소하지 못하도록 예외를 발생시킨다.
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     * 전체 주문 가격을 알려면 각각의 주문상품 가격을 알아야 한다.
+     * 로직을 보면 연관된 주문상품들의 가격을 조회해서 더한 값을 반환한다.
+     * (실무에서는 주로 주문에 전체 주문 가격 필드를 두고 역정규화 한다.)
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -26,4 +26,39 @@ public class OrderItem {
 
     private int orderPrice;     // 주문 가격
     private int count;          // 주문 수량
+
+    //==생성 메서드==//
+
+    /**
+     * 주문 상품, 가격, 수량 정보를 사용해서 주문상품 엔티티를 생성한다.
+     * 그리고 `item.removeStock(count)`를 호출해서 주문한 수량만큼 상품의 재고를 줄인다.
+     */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+     * getItem().addStock(count)` 를 호출해서 취소한 주문 수량만큼 상품의 재고를 증가시킨다.
+     */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 주문 상품 전체 가격 조회
+     * 주문 가격에 수량을 곱한 값을 반환한다.
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
