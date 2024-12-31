8e04e25c715adf5dff5979c31a4d2da8411d54e8
nickhealthy <alskadmlcraz1@gmail.com>
2024-12-31 13:21:27 +0900

    4. 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,17 +3,21 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
     //엔티티 메니저(EntityManager) 주입
     //`@PersistenceUnit`: 엔티티 메니터 팩토리(EntityManagerFactory) 주입
-    @PersistenceContext
-    private EntityManager em;
+//    @PersistenceContext
+//    private EntityManager em;
+
+    private final EntityManager em; // 스프링 데이터 JPA를 사용하면 `EntityManager`도 주입가능
 
     public void save(Member member) {
         em.persist(member);
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+/*
+* 트랜잭션, 영속성 컨텍스트
+* `readOnly = true`
+* - 데이터의 변경이 없는 읽기 전용 메서드에 사용
+* - 영속성 컨텍스트를 플러시 하지 않으므로 약간의 성능 향상(읽기 전용에는 다 적용)
+* - 데이터베이스 드라이버가 지원하면 DB에서 성능 향샹
+* */
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    @Transactional // 쓰기 작업
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers =
+                memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+}
