bdb04ba66b051d865c1c02c8082a6b4088837aab
nickhealthy <alskadmlcraz1@gmail.com>
2025-01-04 16:59:49 +0900

    6. 주문 기능 테스트

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -8,8 +8,9 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import jpabook.jpashop.repository.OrderRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -54,6 +55,42 @@ public class OrderServiceTest {
         assertEquals("주문 가격은 가격 * 수량이다.", 10000 * 2, getOrder.getTotalPrice());
         assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
     }
+
+    @Test(expected = NotEnoughStockException.class)
+    public void 상품주문_재고수량초과() {
+
+        //given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
+
+        //when
+        int orderCount = 11; //재고보다 많은 수량
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        //then
+        fail("재고 수량 부족 예외가 발생해야 한다.");
+    }
+
+    @Test
+    public void 주문취소() {
+
+        //given(주문 설정)
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10); //이름, 가격, 재고
+        int orderCount = 2;
+
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        //when(주문 취소)
+        orderService.cancelOrder(orderId);
+
+        //then(주문 상태가 주문 취소 상태인지, 취소한 만큼 재고가 증가했는지 검증)
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("주문 취소시 상태는 CANCEL 이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 금나큼 재고가 증가해야 한다.", 10, item.getStockQuantity());
+    }
+
 
     private Book createBook(String name, int price, int stockQuantity) {
         Book book = new Book();
