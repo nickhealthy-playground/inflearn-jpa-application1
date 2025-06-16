4746a4a4c08e6212879cefb16f879d59a3407141
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-16 22:57:14 +0900

    3. 도메인 분석 설계 - 엔티티 클래스 개발1

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.*;
+
+@Table(name = "orders")
+@Entity
+@Getter @Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(fetch = LAZY, mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
