771ccb1fbf5df42038a5f7f76c9af126cdae07e3
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-17 00:15:01 +0900

    3. 도메인 분석 설계 - 엔티티 설계시 주의점

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -24,15 +24,31 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(fetch = LAZY, mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus status;
+    private OrderStatus status; // 주문 상태[ORDER, CANCLE]
+
+    // == 연관관계 편의 메서드 ==
+    public void setMember(Member member) {
+        this.setMember(member);
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.setDelivery(delivery);
+        delivery.setOrder(this);
+    }
 }
