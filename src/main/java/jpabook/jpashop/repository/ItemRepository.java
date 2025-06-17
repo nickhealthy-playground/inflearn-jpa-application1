8556fb37de585dbb3ee61dd2e1d9890232d96c01
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-17 15:56:05 +0900

    6. 상품 도메인 개발 - 상품 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 상품 등록
+     */
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    /**
+     * 상품 조회
+     */
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+
+    /**
+     * 상품 전체 조회
+     */
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+}
