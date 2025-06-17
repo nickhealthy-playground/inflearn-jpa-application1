745e23140a17c7c8e13be36f57ef490c56d3952b
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-17 09:32:58 +0900

    5. 회원 도메인 개발 - 회원 리포지토리 개발

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+}
