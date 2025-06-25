5082c4f1365b2c4feb886be8816cb6d2213baa09
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-25 18:08:21 +0900

    [V2] 3.API 개발 고급 준비 - 조회용 샘플 데이터 입력

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,38 +1,13 @@
 package jpabook.jpashop;
 
-import jakarta.annotation.PostConstruct;
-import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.repository.MemberRepository;
-import jpabook.jpashop.service.MemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
 public class JpashopApplication {
 
-	private final MemberService memberService;
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 
-	public JpashopApplication(MemberService memberService) {
-		this.memberService = memberService;
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
-
-	@PostConstruct
-	public void initialize() {
-		Member member1 = new Member();
-		member1.setUsername("USER1");
-		member1.setAddress(new Address("서울", "노원", "123-123"));
-
-		Member member2 = new Member();
-		member2.setUsername("USER2");
-		member2.setAddress(new Address("서울", "노원", "123-123"));
-
-		memberService.join(member1);
-		memberService.join(member2);
-	}
 }
