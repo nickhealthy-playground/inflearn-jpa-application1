62656ebbb50732330c02079c77a6ba3e9e60b4b1
nickhealthy <alskadmlcraz1@gmail.com>
2025-06-26 19:07:38 +0900

    [V2] 5. API 개발 고급 컬렉션 조회 최적화 - 주문 조회 V1: 엔티티 직접 노출

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출 - 좋은 방법X
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getUsername(); // LAZY 강제 초기화
+            order.getDelivery().getAddress(); // LAZY 강제 초기화
+
+            // orderItem, item 관계를 직접 초기화하면 Hibernate5JakartaModule 설정에 의해 엔티티를 JSON 으로 생성한다.
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); // LAZY 강제 초기화
+        }
+
+        return all;
+    }
+}
